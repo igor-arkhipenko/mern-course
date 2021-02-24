55e58afdde34809ea5d023f5bcdcee6aa955f712
igor-arkhipenko <i.arkhipenko@crystals.ru>
2021-02-24 18:31:13 +0300

    work with requests

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react'
+import {useHttp} from '../hooks/http.hook'
 
 export const AuthPage = () => {
+  const {loading, error, request} = useHttp()
   const [form, setForm] = useState({ email: '', password: '' })
 
   const changeHandler = event => {
     setForm({ ...form, [event.target.name]: event.target.value })
+  }
+
+  const registerHandler = async () => {
+    try {
+      const data = await request('/api/auth/register', 'POST', {...form})
+      console.log('data = ', data)
+    } catch (e) {
+      
+    }
   }
 
   return (
@@ -42,8 +53,20 @@ export const AuthPage = () => {
             </div>
           </div>
           <div className="card-action">
-            <button className="btn yellow darken-4" style={{ marginRight: 10 }}>Войти</button>
-            <button className="btn grey darken-1 black-text">Регистрация</button>
+            <button
+              className="btn yellow darken-4"
+              style={{ marginRight: 10 }}
+              disabled={loading}
+            >
+              Войти
+            </button>
+            <button
+              className="btn grey darken-1 black-text"
+              onClick={registerHandler}
+              disabled={loading}
+            >
+              Регистрация
+            </button>
           </div>
         </div>
       </div>
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -16,6 +16,7 @@ router.post(
   async (req, res) => {
     try {
       const errors = validationResult(req)
+
       if (!errors.isEmpty()) {
         return res.status(400).json({
           errors: errors.array(),
