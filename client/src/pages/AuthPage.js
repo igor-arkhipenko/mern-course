cb15d97e9d97cb385a98448031bf8c614c9e1ac7
igor-arkhipenko <i.arkhipenko@crystals.ru>
2021-02-24 17:24:18 +0300

    AuthPage improved

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AuthPage = () => {
+  const [form, setForm] = useState({ email: '', password: '' })
+
+  const changeHandler = event => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+  }
+
   return (
-    <div>
-      <h1>Auth Page</h1>
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h1>Сократи ссылку</h1>
+        <div className="card blue darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">Авторизация</span>
+            <div>
+
+              <div className="input-field">
+                <input
+                  placeholder="Введите email"
+                  id="email"
+                  type="text"
+                  name="email"
+                  className="yellow-input"
+                  onChange={changeHandler}
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+
+              <div className="input-field">
+                <input
+                  placeholder="Введите пароль"
+                  id="password"
+                  type="password"
+                  name="password"
+                  onChange={changeHandler}
+                />
+                <label htmlFor="email">Пароль</label>
+              </div>
+
+            </div>
+          </div>
+          <div className="card-action">
+            <button className="btn yellow darken-4" style={{ marginRight: 10 }}>Войти</button>
+            <button className="btn grey darken-1 black-text">Регистрация</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
