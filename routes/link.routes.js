833ee4688208caef356f9343f876d4a72ab12306
igor-arkhipenko <i.arkhipenko@crystals.ru>
2021-02-25 17:13:40 +0300

    work with backend's business logic

---
+++ routes/link.routes.js
@@ -0,0 +1,49 @@
+const {Router} = require('express')
+const Link = require('../models/Link')
+const shortid = require('shortid')
+const auth = require('../middleware/auth.middleware')
+const config = require('config')
+const router = Router()
+
+router.post('/generate', auth, async (req, res) => {
+  try {
+    const baseUrl = config.get('baseUrl')
+    const {from} = req.body
+    const code = shortid.generate()
+    const existing = await Link.findOne({ from })
+
+    if (existing) {
+      return res.json({ link: existing })
+    }
+
+    const to = beseUrl + '/t/' + code
+    const link = new Link({
+      code, from, to, owner: req.user.userId
+    })
+
+    await link.save()
+    res.status(201).json({ link })
+  } catch (e) {
+    res.status(500).json({ message: 'Упс, что-то пошло не так...' })
+  }
+})
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const links = await Link.find({ owner: req.user.userId })
+    res.json(links)
+  } catch (e) {
+    res.status(500).json({ message: 'Упс, что-то пошло не так...' })
+  }
+})
+
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const link = await Link.findById(req.params.id) // ???
+    res.json(link)
+  } catch (e) {
+    res.status(500).json({ message: 'Упс, что-то пошло не так...' })
+  }
+})
+
+module.exports = router
